9b595f467180697addcd988dbfba9433a0c0e121
Andres Carrillo <andresdavidcm24@gmail.com>
2020-09-05 23:36:12 -0500

    Solving others errors

--- src/controllers/init_controller.ts
+++ src/controllers/init_controller.ts
@@ -3,10 +3,13 @@ import {tsconfig} from '../tools/tsconfig';
 import {packageConfig} from '../tools/package';
 import {indexData} from '../tools/index';
 import { userControllerData } from '../tools/src/user_controller';
-import { indexRoutesData } from '../tools/src/user_routes';
+import { userRoutesData } from '../tools/src/user_routes';
 import { userModelData } from '../tools/src/user_model';
 import { appData } from '../tools/app';
 import { databaseData } from '../tools/database';
+
+const path:string = `data/`;
+// const path:string = `data/`;
 
 class InitController {
 
@@ -14,32 +17,32 @@ class InitController {
 
         console.log(`Initializing ${name}`);
 
-        this.createFile('./tsconfig.json', JSON.stringify(tsconfig, null, 4));
+        this.createFile(`./${path}tsconfig.json`, JSON.stringify(tsconfig, null, 4));
         
         packageConfig.name = name;
         
-        this.createFile('./package.json', JSON.stringify(packageConfig, null, 4));
+        this.createFile(`./${path}package.json`, JSON.stringify(packageConfig, null, 4));
 
-        fs.mkdir('./src/', { recursive: true }, (err) => {
+        fs.mkdir(`./${path}src/`, { recursive: true }, (err) => {
             if (err) throw err;
-            this.createFile('./src/index.ts', indexData);
-            this.createFile('./src/app.ts', appData);
-            this.createFile('./src/database.ts', databaseData);
+            this.createFile(`./${path}src/index.ts`, indexData);
+            this.createFile(`./${path}src/app.ts`, appData);
+            this.createFile(`./${path}src/database.ts`, databaseData);
         });
 
-        fs.mkdir('./src/controllers/', { recursive: true }, (err) => {
+        fs.mkdir(`./${path}src/controllers/`, { recursive: true }, (err) => {
             if (err) throw err;
-            this.createFile('./src/controllers/user_controller.ts', userControllerData);
+            this.createFile(`./${path}src/controllers/user_controller.ts`, userControllerData);
         });
 
-        fs.mkdir('./src/routes/', { recursive: true }, (err) => {
+        fs.mkdir(`./${path}src/routes/`, { recursive: true }, (err) => {
             if (err) throw err;
-            this.createFile('./src/routes/user_routes.ts', indexRoutesData);
+            this.createFile(`./${path}src/routes/user_routes.ts`, userRoutesData);
         });
 
-        fs.mkdir('./src/models/', { recursive: true }, (err) => {
+        fs.mkdir(`./${path}src/models/`, { recursive: true }, (err) => {
             if (err) throw err;
-            this.createFile('./src/models/user_model.ts', userModelData);
+            this.createFile(`./${path}src/models/user_model.ts`, userModelData);
         });
 
         console.log(`${name} Inicializated`);
@@ -47,7 +50,7 @@ class InitController {
     
     private createFile = (path:string, data:string):void => {
         fs.writeFile(path, data, (err) => {
-            if(err) throw new Error('Can\'t create tsconfig file');
+            if(err) throw new Error(`Can\`t create tsconfig file`);
         });
     }
 }
