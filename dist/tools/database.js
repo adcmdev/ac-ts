8c2d0dc5968e1bc84cfcef3f0f5bef155a3076b5
Andres Carrillo <andresdavidcm24@gmail.com>
2020-09-06 20:54:40 -0500

    Solving env error

--- dist/tools/database.js
+++ dist/tools/database.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.databaseData = void 0;
-exports.databaseData = "import mongoose from 'mongoose';\n\nmongoose.connect(process.env.TOKEN_SECRET, {\n    useUnifiedTopology: true,\n    useNewUrlParser: true,\n    useCreateIndex: true\n}, (err) => {\n    if(err) return err.message;\n    console.log('Database is online');\n});";
+exports.databaseData = "import mongoose from 'mongoose';\n\nmongoose.connect(process.env.MONGO_URI + '', {\n    useUnifiedTopology: true,\n    useNewUrlParser: true,\n    useCreateIndex: true\n}, (err) => {\n    if(err) return console.log(err.message);\n    console.log('Database is online');\n});";
 //# sourceMappingURL=database.js.map
