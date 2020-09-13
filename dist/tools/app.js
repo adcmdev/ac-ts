2fc08e94d8ed4b74fc58524c789931877ba25442
Andres Carrillo <andresdavidcm24@gmail.com>
2020-09-13 00:07:21 -0500

    Solving errors

--- dist/tools/app.js
+++ dist/tools/app.js
@@ -5,7 +5,7 @@ exports.appData = (ws) => {
     const http = (ws) ? "import http from 'http';\n" : "";
     const path = (ws) ? "import path from 'path';\n\n" : "\n";
     const server = (ws) ? "export let server:http.Server = http.createServer(app);\n\n" : "";
-    const publicPath = (ws) ? "const publicPath:string = path.resolve(__dirname, '../src/public');\n\n" : "";
+    const publicPath = (ws) ? "const publicPath:string = path.resolve(__dirname, '../public');\n\n" : "";
     const middelware = (ws) ? "app.use('/', express.static(publicPath));\n\n" : "";
     let data = "import express, {Application} from 'express';\n" +
         "import userRoutes from './routes/user_routes';\n" +
--- dist/tools/src/socket_routes.js
+++ dist/tools/src/socket_routes.js
@@ -10,7 +10,7 @@ exports.socketRoutesData = "import ws from 'ws'\n" +
     "wsServer.on('connection', socketController.onConnect);\n\n" +
     "server.on('upgrade', (request:Request, socket, head) => {\n\n" +
     "    const pathname:string = url.parse(request.url).pathname + '';\n\n" +
-    "    if(pathname === '/connection/') {\n\n" +
+    "    if(pathname === '/connection') {\n\n" +
     "        wsServer.handleUpgrade(request, socket, head, socket => {\n" +
     "            wsServer.emit('connection', socket, request);\n" +
     "        });\n" +
